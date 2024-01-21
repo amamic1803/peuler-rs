4c908cdff07f4e776a0292df68341009b786a1df
DarkLord76865 <mmima191@gmail.com>
2024-01-21 15:45:45 +0100

    improve Sieve of Eratosthenes

--- src/shared/math.rs
+++ src/shared/math.rs
@@ -531,40 +531,38 @@ pub fn sieve_of_eratosthenes(n: u64) -> Vec<u64> {
         0..=1 => Vec::with_capacity(0),
         2 => vec![2],
         _ => {
-            let mut result = vec![2];
-            let mut sieve = vec![true; if n % 2 == 0 { n - 2 } else { n - 1 } as usize / 2];
+            let mut results = vec![2];
+            let mut sieve = vec![true; (n - 1) as usize / 2];
 
-            let mut prime_index = 0;
-            let ind_to_val = |i: usize| 3 + 2 * i as u64;
-            let val_to_ind = |v: u64| (v as usize - 3) / 2;
+            let ind_to_val = |i: usize| ((i as u64) << 1) + 3;  // calculate number value from index in sieve
+            let val_to_ind = |v: u64| ((v - 3) >> 1) as usize;  // calculate index in sieve from number value
 
-            loop {
-                let prime_value = ind_to_val(prime_index);
-                let mut current_value = prime_value * prime_value;
-                let mut current_position = val_to_ind(current_value);
-                if current_position >= sieve.len() {
-                    break;
-                }
-                while current_position < sieve.len() {
-                    sieve[current_position] = false;
-                    current_value += 2 * prime_value;
-                    current_position = val_to_ind(current_value);
-                }
+            for prime_ind in 0..sieve.len() {
+                if sieve[prime_ind] {
+                    // get prime number value
+                    let prime_val = ind_to_val(prime_ind);
 
-                prime_index += 1;
-                while prime_index < sieve.len() && !sieve[prime_index] {
-                    prime_index += 1;
+                    // check all multiples of prime number value and mark them as not prime
+                    // start checking at prime_val^2 (all smaller multiples have already been checked by smaller primes)
+                    let mut check_val = prime_val * prime_val;
+                    let mut check_ind = val_to_ind(check_val);
+                    if check_ind >= sieve.len() { break; }
+
+                    while check_ind < sieve.len() {
+                        sieve[check_ind] = false;
+                        // we want check_val to always be odd, prime_val is always odd so we can just add prime_val * 2
+                        // (because if we added 2 odd numbers we would get an even number)
+                        check_val += prime_val << 1;
+                        check_ind = val_to_ind(check_val);
+                    }
                 }
             }
 
-            result.extend(
-                sieve
-                    .iter()
-                    .enumerate()
-                    .filter(|(_, &is_prime)| is_prime)
-                    .map(|(i, _)| (i as u64) * 2 + 3)
-            );
-            result
+            // convert sieve indices that are true to their corresponding number values and add them to results
+            results.extend(sieve.into_iter().enumerate().filter_map(|(i, prime)| if prime { Some(ind_to_val(i)) } else { None }));
+
+            // return results
+            results
         },
     }
 }
