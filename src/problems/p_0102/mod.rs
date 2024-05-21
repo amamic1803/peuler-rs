e5be85fc8dfb4691e6b17770f3fcf797ad3e6604
DarkLord76865 <mmima191@gmail.com>
2024-05-21 16:55:21 +0200

    replace redundant functions with generic ones

--- src/problems/p_0102/mod.rs
+++ src/problems/p_0102/mod.rs
@@ -6,7 +6,7 @@ pub fn get_problem() -> Problem {
     Problem::new(102, "Triangle Containment", solve)
 }
 
-use crate::shared::math::{Point2D, Vector2D};
+use crate::shared::math::{Point, Vector};
 use std::f64::consts::PI;
 
 const FULL_ANGLE: f64 = 2.0 * PI;
@@ -25,9 +25,9 @@ fn solve() -> String {
 
     for triangle in triangles {
         let mut angle_sum = 0.0;
-        let vec1 = Vector2D::from_points(Point2D::new(0.0, 0.0), triangle.0);
-        let vec2 = Vector2D::from_points(Point2D::new(0.0, 0.0), triangle.1);
-        let vec3 = Vector2D::from_points(Point2D::new(0.0, 0.0), triangle.2);
+        let vec1 = Vector::from_points(Point::new([0.0; 2]), triangle[0]);
+        let vec2 = Vector::from_points(Point::new([0.0; 2]), triangle[1]);
+        let vec3 = Vector::from_points(Point::new([0.0; 2]), triangle[2]);
 
         angle_sum += vec1.angle_between(&vec2);
         angle_sum += vec2.angle_between(&vec3);
@@ -41,17 +41,17 @@ fn solve() -> String {
     count.to_string()
 }
 
-fn parse_input(input: &str) -> Vec<(Point2D, Point2D, Point2D)> {
+fn parse_input(input: &str) -> Vec<[Point<2>; 3]> {
     input
         .trim()
         .lines()
         .map(|line| {
             let mut line_iter = line.trim().split(',').map(|num_str| num_str.parse::<f64>().unwrap());
-            (
-                Point2D::new(line_iter.next().unwrap(), line_iter.next().unwrap()),
-                Point2D::new(line_iter.next().unwrap(), line_iter.next().unwrap()),
-                Point2D::new(line_iter.next().unwrap(), line_iter.next().unwrap()),
-            )
+            [
+                Point::new([line_iter.next().unwrap(), line_iter.next().unwrap()]),
+                Point::new([line_iter.next().unwrap(), line_iter.next().unwrap()]),
+                Point::new([line_iter.next().unwrap(), line_iter.next().unwrap()]),
+            ]
         })
-        .collect::<Vec<(Point2D, Point2D, Point2D)>>()
+        .collect()
 }
--- src/shared/math.rs
+++ src/shared/math.rs
@@ -7,6 +7,7 @@ use once_cell::sync::Lazy;
 use std::collections::HashSet;
 use std::iter;
 use std::mem;
+use std::ops::{Add, Mul, Sub};
 use std::sync::Mutex;
 
 /// Inverse of the prime-counting function.
@@ -623,33 +624,25 @@ pub fn phi_1_to_n(n: u64) -> Vec<u64> {
     phi_values
 }
 
-/// Represents a point in 2D space.
-pub struct Point2D {
-    /// The x coordinate.
-    pub x: f64,
-    /// The y coordinate.
-    pub y: f64,
+/// A point in N-dimensional space.
+/// # Example
+/// ```
+/// use project_euler::shared::math::Point;
+/// let point = Point::new([1.0, 2.0, 3.0]);
+/// assert_eq!(point.coords, [1.0, 2.0, 3.0]);
+/// ```
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Point<const N: usize> {
+    pub coords: [f64; N],
 }
-impl Point2D {
+impl<const N: usize> Point<N> {
     /// Creates a new point.
-    pub fn new(x: f64, y: f64) -> Self {
-        Self { x, y }
-    }
-}
-
-/// Represents a point in 3D space.
-pub struct Point3D {
-    /// The x coordinate.
-    pub x: f64,
-    /// The y coordinate.
-    pub y: f64,
-    /// The z coordinate.
-    pub z: f64,
-}
-impl Point3D {
-    /// Creates a new point.
-    pub fn new(x: f64, y: f64, z: f64) -> Self {
-        Self { x, y, z }
+    /// # Arguments
+    /// * `coords` - The coordinates of the point.
+    /// # Returns
+    /// * `Point` - The new point.
+    pub fn new(coords: [f64; N]) -> Self {
+        Self { coords }
     }
 }
 
@@ -998,92 +991,184 @@ pub fn slice_to_int(n: &[u8]) -> u64 {
     sum
 }
 
-/// Represents a vector in 2D space.
-pub struct Vector2D {
-    /// The factor of the x unit vector.
-    pub x: f64,
-    /// The factor of the y unit vector.
-    pub y: f64,
+/// A vector in N-dimensional space.
+/// # Example
+/// ```
+/// use project_euler::shared::math::Vector;
+///
+/// let vector = Vector::new([1.0, 2.0, 3.0]);
+/// assert_eq!(vector.coords, [1.0, 2.0, 3.0]);
+///
+/// let quad_vector = 2.0 * vector * 2.0;
+/// assert_eq!(quad_vector.coords, [4.0, 8.0, 12.0]);
+///
+/// let vector2 = Vector::new([4.0, 5.0, 6.0]);
+/// let add_vector = vector + vector2;
+/// let sub_vector = vector - vector2;
+/// assert_eq!(add_vector.coords, [5.0, 7.0, 9.0]);
+/// assert_eq!(sub_vector.coords, [-3.0, -3.0, -3.0]);
+/// ```
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Vector<const N: usize> {
+    pub coords: [f64; N],
 }
-impl Vector2D {
+impl<const N: usize> Vector<N> {
     /// Creates a new vector.
-    pub fn new(x_factor: f64, y_factor: f64) -> Self {
-        Self { x: x_factor, y: y_factor }
+    /// # Arguments
+    /// * `coords` - The coordinates of the vector.
+    /// # Returns
+    /// * `Vector` - The vector.
+    pub fn new(coords: [f64; N]) -> Self {
+        Self { coords }
     }
-
-    /// Creates a new vector from two points.
-    pub fn from_points(point1: Point2D, point2: Point2D) -> Self {
-        Self {
-            x: point2.x - point1.x,
-            y: point2.y - point1.y,
+    /// Creates a new vector from 2 points.
+    /// # Arguments
+    /// * `point1` - The first point.
+    /// * `point2` - The second point.
+    /// # Returns
+    /// * `Vector` - The vector from point1 to point2.
+    /// # Example
+    /// ```
+    /// use project_euler::shared::math::{Point, Vector};
+    /// let point1 = Point::new([1.0, 2.0, 3.0]);
+    /// let point2 = Point::new([4.0, 5.0, 6.0]);
+    /// let vector = Vector::from_points(point1, point2);
+    /// assert_eq!(vector.coords, [3.0, 3.0, 3.0]);
+    /// ```
+    pub fn from_points(point1: Point<N>, point2: Point<N>) -> Self {
+        let mut coords = [0.0; N];
+        for (i, coord) in coords.iter_mut().enumerate() {
+            *coord = point2.coords[i] - point1.coords[i];
         }
+        Self { coords }
     }
-
-    /// Calculates the magnitude of the vector.
-    pub fn magnitude(&self) -> f64 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
-    }
-
-    /// Calculates the dot product of two vectors.
-    pub fn dot_product(&self, other: &Self) -> f64 {
-        self.x * other.x + self.y * other.y
-    }
-
     /// Calculates the angle between two vectors.
+    /// The angle is in radians.
+    /// # Arguments
+    /// * `other` - The other vector.
+    /// # Returns
+    /// * `f64` - The angle between the two vectors.
+    /// # Panics
+    /// If at least one of the vectors is zero.
+    /// # Example
+    /// ```
+    /// use project_euler::shared::math::Vector;
+    /// let vector1 = Vector::new([1.0, 0.0]);
+    /// let vector2 = Vector::new([0.0, 1.0]);
+    /// assert_eq!(vector1.angle_between(&vector2), std::f64::consts::FRAC_PI_2);
+    /// ```
     pub fn angle_between(&self, other: &Self) -> f64 {
         (self.dot_product(other) / (self.magnitude() * other.magnitude())).acos()
     }
-}
-
-/// Represents a vector in 3D space.
-pub struct Vector3D {
-    /// The factor of the x unit vector.
-    pub x: f64,
-    /// The factor of the y unit vector.
-    pub y: f64,
-    /// The factor of the z unit vector.
-    pub z: f64,
-}
-impl Vector3D {
-    /// Creates a new vector.
-    pub fn new(x_factor: f64, y_factor: f64, z_factor: f64) -> Self {
-        Self {
-            x: x_factor,
-            y: y_factor,
-            z: z_factor,
-        }
-    }
-
-    /// Creates a new vector from two points.
-    pub fn from_points(point1: Point3D, point2: Point3D) -> Self {
-        Self {
-            x: point2.x - point1.x,
-            y: point2.y - point1.y,
-            z: point2.z - point1.z,
-        }
-    }
-
-    /// Calculates the magnitude of the vector.
-    pub fn magnitude(&self) -> f64 {
-        (self.x.powi(2) + self.y.powi(2) + self.z.powi(2)).sqrt()
-    }
-
-    /// Calculates the dot product of two vectors.
-    pub fn dot_product(&self, other: &Self) -> f64 {
-        self.x * other.x + self.y * other.y + self.z * other.z
-    }
-
-    /// Calculates the angle between two vectors.
-    pub fn angle_between(&self, other: &Self) -> f64 {
-        (self.dot_product(other) / (self.magnitude() * other.magnitude())).acos()
-    }
-
     /// Calculates the cross product of two vectors.
+    /// Only defined for 3D vectors.
+    /// # Arguments
+    /// * `other` - The other vector.
+    /// # Returns
+    /// * `Vector` - The cross product of the two vectors.
+    /// # Example
+    /// ```
+    /// use project_euler::shared::math::Vector;
+    /// let vector1 = Vector::new([1.0, 0.0, 0.0]);
+    /// let vector2 = Vector::new([0.0, 1.0, 0.0]);
+    /// assert_eq!(vector1.cross_product(&vector2).coords, [0.0, 0.0, 1.0]);
+    /// ```
     pub fn cross_product(&self, other: &Self) -> Self {
-        Self {
-            x: self.y * other.z - self.z * other.y,
-            y: self.z * other.x - self.x * other.z,
-            z: self.x * other.y - self.y * other.x,
+        assert_eq!(N, 3, "Cross product is only defined for 3D vectors.");
+        let mut coords = [0.0; N];
+        coords[0] = self.coords[1] * other.coords[2] - self.coords[2] * other.coords[1];
+        coords[1] = self.coords[2] * other.coords[0] - self.coords[0] * other.coords[2];
+        coords[2] = self.coords[0] * other.coords[1] - self.coords[1] * other.coords[0];
+        Self { coords }
+    }
+    /// Calculates the dot product of two vectors.
+    /// # Arguments
+    /// * `other` - The other vector.
+    /// # Returns
+    /// * `f64` - The dot product of the two vectors.
+    /// # Example
+    /// ```
+    /// use project_euler::shared::math::Vector;
+    /// let vector1 = Vector::new([1.0, 2.0, 3.0]);
+    /// let vector2 = Vector::new([4.0, 5.0, 6.0]);
+    /// assert_eq!(vector1.dot_product(&vector2), 32.0);
+    /// ```
+    pub fn dot_product(&self, other: &Self) -> f64 {
+        self.coords.iter().zip(other.coords.iter()).map(|(&x, &y)| x * y).sum()
+    }
+    /// Calculates the magnitude of the vector.
+    /// # Returns
+    /// * `f64` - The magnitude of the vector.
+    /// # Example
+    /// ```
+    /// use project_euler::shared::math::Vector;
+    /// let vector = Vector::new([1.0, 2.0, 3.0]);
+    /// assert_eq!(vector.magnitude(), 3.7416573867739413);
+    /// ```
+    pub fn magnitude(&self) -> f64 {
+        self.coords.iter().map(|&x| x.powi(2)).sum::<f64>().sqrt()
+    }
+    /// Calculates the normalized vector.
+    /// # Returns
+    /// * `Vector` - The normalized vector.
+    /// # Panics
+    /// If the vector is zero.
+    /// # Example
+    /// ```
+    /// use project_euler::shared::math::Vector;
+    /// let vector = Vector::new([1.0, 2.0, 3.0]);
+    /// assert_eq!(vector.normalize().coords, [0.2672612419124244, 0.5345224838248488, 0.8017837257372732]);
+    /// ```
+    pub fn normalize(&self) -> Self {
+        let mag = self.magnitude();
+        let mut coords = [0.0; N];
+        for (i, coord) in coords.iter_mut().enumerate() {
+            *coord = self.coords[i] / mag;
         }
+        Self { coords }
+    }
+}
+impl<const N: usize> Add for Vector<N> {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        let mut coords = [0.0; N];
+        for (i, coord) in coords.iter_mut().enumerate() {
+            *coord = self.coords[i] + other.coords[i];
+        }
+        Self { coords }
+    }
+}
+impl<const N: usize> Sub for Vector<N> {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        let mut coords = [0.0; N];
+        for (i, coord) in coords.iter_mut().enumerate() {
+            *coord = self.coords[i] - other.coords[i];
+        }
+        Self { coords }
+    }
+}
+impl<const N: usize> Mul<f64> for Vector<N> {
+    type Output = Vector<N>;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        let mut coords = self.coords;
+        for coord in coords.iter_mut() {
+            *coord *= rhs;
+        }
+        Self { coords }
+    }
+}
+impl<const N: usize> Mul<Vector<N>> for f64 {
+    type Output = Vector<N>;
+
+    fn mul(self, rhs: Vector<N>) -> Self::Output {
+        let mut coords = rhs.coords;
+        for coord in coords.iter_mut() {
+            *coord *= self;
+        }
+        Vector::<N> { coords }
     }
 }
